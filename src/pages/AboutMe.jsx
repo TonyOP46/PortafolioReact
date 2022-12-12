8be53d42ceb472f1d50e9a21b7174eaa66370c6a
TonyOP46 <88261670+TonyOP46@users.noreply.github.com>
2022-12-12 13:22:33 -0500

    update

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -15,17 +15,30 @@ const AboutMe = () => {
             <h2>About me</h2>
             <div className="dividir"></div>
             <p>
-                As an engineer I am passionate about software development through the JavaScripts programming language, I have developed in HTML, CSS, Reactjs, Redux Nodejs, express and Python. <br />  
-                I consider myself a manager
-                person who tries to do his job in the best way
-                possible.
+              As an engineer I am passionate about software development through
+              the JavaScripts programming language, I have developed in HTML,
+              CSS, Reactjs, Redux Nodejs, express and Python. <br />I consider
+              myself a manager person who tries to do his job in the best way
+              possible.
             </p>
             <p>
               I let myself be defeated, currently I don't have much experience
               but I have all the desire to learn and to follow getting ready, I
               like it make new friends and help those most need
             </p>
-            <button className="Great">Great</button>
+            <div className="social">
+              <div className="social-icons">
+                <a href="https://github.com/TonyOP46" target="_blanc">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/tony-ospino-parra-276454167/"
+                  target="_blanc"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,69 +1,93 @@
 import React from "react";
 import "../Portfolio.css";
-import ecommerce from '../PROJECTS/E-COMMERCE.png'
-import Rick from '../PROJECTS/rick.jpg'
-import pokemon from '../PROJECTS/Pokemon.png'
+import ecommerce from "../PROJECTS/E-COMMERCE.png";
+import Rick from "../PROJECTS/rick.jpg";
+import pokemon from "../PROJECTS/Pokemon.png";
 import { Container } from "react-bootstrap";
 
 const Portfolio = () => {
-
   return (
     <div class="container">
-        <div        className="portfolio-card" >
-                <div className="ecommerce">
-                    <img src={ecommerce} alt="" />
-                </div>
-                <div className="social">
-                    <h2>E-Commerce</h2>
-                    <a href="https://github.com/TonyOP46" target="_blanc">
-                    <i className="fa-brands fa-github">
-                        </i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/tony-ospino-parra-276454167/" target="_blanc">
-                    <i className="fa-brands fa-linkedin"></i>
-                    </a> 
-                </div>
-                <button>
-                    <a href="https://quiet-palmier-ca789e.netlify.app/" target="_blanc">Project Detail</a>
-                </button>
+      <div className="portfolio-card">
+        <div className="ecommerce">
+          <img src={ecommerce} alt="" />
         </div>
-        <div className="portfolio-card" >
-                <div className="ecommerce2" style={{objectFit: 'cover'}}>
-                    <img src={Rick} alt="" />
-                </div>
-                <div className="social">
-                    <h2>Rick And Morty</h2>
-                    <a href="https://github.com/TonyOP46" target="_blanc">
-                    <i className="fa-brands fa-github">
-                        </i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/tony-ospino-parra-276454167/" target="_blanc">
-                    <i className="fa-brands fa-linkedin"></i>
-                    </a> 
-                </div>
-                <button>
-                    <a href="https://sprightly-licorice-d52372.netlify.app/" target="_blanc">Project Detail</a>
-                </button>
-                
+        <div className="social">
+          <h2>E-Commerce</h2>
+          <div className="social-icons">
+            <div className="icon1">
+            <i className="fa-brands fa-html5"></i>
+            </div>
+            <div className="icon2">
+            <i className="fa-brands fa-react"></i>
+            </div>
+            <div className="icon3">
+            <i className="fa-brands fa-css3-alt"></i>
+            </div>
+            
+          </div>
         </div>
-        <div className="portfolio-card" >
-                <div className="ecommerce3">
-                    <img src={pokemon} alt="" />
-                </div>
-                <div className="social">
-                    <h2>PokeApi</h2>
-                    <a href="https://github.com/TonyOP46" target="_blanc">
-                    <i className="fa-brands fa-github">
-                        </i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/tony-ospino-parra-276454167/" target="_blanc">
-                    <i className="fa-brands fa-linkedin"></i>
-                    </a>  
-                </div>
-                <button>
-                    <a href="https://magical-banoffee-5f4dbd.netlify.app/" target="_blanc">Project Detail</a>
-                </button>
+        <button>
+          <a href="https://quiet-palmier-ca789e.netlify.app/" target="_blanc">
+            Project Detail
+          </a>
+        </button>
+      </div>
+      <div className="portfolio-card">
+        <div className="ecommerce2" style={{ objectFit: "cover" }}>
+          <img src={Rick} alt="" />
         </div>
+        <div className="social">
+          <h2>Rick And Morty</h2>
+          <div className="social-icons">
+          <div className="icon1">
+            <i className="fa-brands fa-html5"></i>
+            </div>
+            <div className="icon2">
+            <i className="fa-brands fa-react"></i>
+            </div>
+            <div className="icon3">
+            <i className="fa-brands fa-css3-alt"></i>
+            </div>
+          </div>
+        </div>
+        <button>
+          <a
+            href="https://sprightly-licorice-d52372.netlify.app/"
+            target="_blanc"
+          >
+            Project Detail
+          </a>
+        </button>
+      </div>
+      <div className="portfolio-card">
+        <div className="ecommerce3">
+          <img src={pokemon} alt="" />
+        </div>
+        <div className="social">
+          <h2>PokeApi</h2>
+          <div className="social-icons">
+          <div className="icon1">
+            <i className="fa-brands fa-html5"></i>
+            </div>
+            <div className="icon2">
+            <i className="fa-brands fa-react"></i>
+            </div>
+            <div className="icon3">
+            <i className="fa-brands fa-css3-alt"></i>
+            </div>
+            
+          </div>
+        </div>
+        <button>
+          <a
+            href="https://magical-banoffee-5f4dbd.netlify.app/"
+            target="_blanc"
+          >
+            Project Detail
+          </a>
+        </button>
+      </div>
     </div>
   );
 };
