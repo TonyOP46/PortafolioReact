1ef74aa52b4d33fb0f184c2271c649c77b110a97
TonyOP46 <88261670+TonyOP46@users.noreply.github.com>
2023-01-09 15:54:39 -0500

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,26 +5,21 @@ import "../Contact.css";
 const Contact = () => {
 
   return (
-    <Form className="form" >
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Send me a message</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <FloatingLabel controlId="floatingTextarea" className="mb-3">
-        Comment
-        <Form.Control
-          as="textarea"
-          placeholder="Leave a comment here"
-          style={{ height: "100px" }}
-        />
-        <Button variant="primary" type="submit" className="button">
+    <div className="form">
+      <div className="form-action">
+      <form action="">
+        <input type="text" name="email" placeholder="Digite su correo electronico"/>
+        <h1>
+          We'll never share your email with anyone else
+        </h1>
+        <p>Comments</p>
+        <textarea name="" id="" cols="50" rows="10"></textarea>
+        <button>
           Submit
-        </Button>
-      </FloatingLabel>
-    </Form>
+        </button>
+      </form>
+      </div>
+    </div>
     
   );
 };
