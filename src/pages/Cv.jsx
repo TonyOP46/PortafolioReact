ad4748221f219683256f19109bbe638b8333cf77
TonyOP46 <88261670+TonyOP46@users.noreply.github.com>
2023-01-10 13:50:27 -0500

    update

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -6,45 +6,45 @@ import { Container } from "react-bootstrap";
 const Cv = () => {
   return (
     <Container>
-        <div class="info">
-          <div className="info-text">
-            <div  className="contain">
+      <div class="info">
+        <div className="info-text">
+          <div className="contain">
             <h1>
               My name is <span className="name">Tony Ospino</span>
             </h1>
-            <p >Developing tomorrow</p>
+            <p>Developing tomorrow</p>
             <p className="parrafo">
-             <h3>I am a web developer</h3>I master
-              technologies such as ReactJs, NodeJs and JavaScripts.
+              <h3>I am a web developer</h3>I master technologies such as
+              ReactJs, NodeJs and JavaScripts.
             </p>
+          </div>
+          <div className="social1">
+            <div className="social-icons1">
+              <a href="https://github.com/TonyOP46" target="_blanc">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/tony-ospino-parra-276454167/"
+                target="_blanc"
+              >
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
             </div>
-            <div className="social1">
-              <div className="social-icons1">
-                <a href="https://github.com/TonyOP46" target="_blanc">
-                  <i className="fa-brands fa-github"></i>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/tony-ospino-parra-276454167/"
-                  target="_blanc"
-                >
-                  <i className="fa-brands fa-linkedin"></i>
-                </a>
-              </div>
-              <div className="social-downloads">
-                <div className="boton">
-                  <button>
+            <div className="social-downloads">
+              <div className="btn">
+                <button>
                   <a href={cv} target="_blanc">
                     Download Cv
                   </a>
-                  </button>
-                </div>
+                </button>
               </div>
             </div>
           </div>
-          <div className="intro">
-            <img src={laptop} alt="" />
-          </div>
         </div>
+        <div className="intro">
+          <img src={laptop} alt="" />
+        </div>
+      </div>
     </Container>
   );
 };
